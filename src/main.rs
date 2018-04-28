c1e91808238a624d306e463d79618a05e8ebb259
Xeryus Stokkel <armadillo@onenetbeyond.org>
2018-04-28 22:00:28 +0200

    Add some command line arguments

--- src/main.rs
+++ src/main.rs
@@ -25,9 +25,23 @@ fn main() {
              .help("The image to create a palette for")
              .required(true)
              .index(1))
+        .arg(Arg::with_name("results")
+             .help("Number of colors to return")
+             .takes_value(true)
+             .short("r")
+             .long("results")
+             .default_value("8"))
+        .arg(Arg::with_name("group_distance")
+             .help("Max Euclidean distance between colors in group")
+             .takes_value(true)
+             .short("d")
+             .long("distance")
+             .default_value("16"))
         .get_matches();
 
     let image_name = matches.value_of("IMAGE").unwrap();
+    let results = value_t!(matches, "results", usize).unwrap();
+    let distance = value_t!(matches, "group_distance", u8).unwrap();
     println!("Loading {}...", image_name);
     let img = image::open(image_name).unwrap();
 
@@ -59,7 +73,7 @@ fn main() {
     count.sort_by_key(|&(_k, v)| v);
     println!("Most common colors");
     for (rank, color) in count.iter().rev().enumerate() {
-        if rank >= 8 {
+        if rank >= results {
             break;
         }
         println!("{:>2}: #{:02x}{:02x}{:02x}",
@@ -69,7 +83,7 @@ fn main() {
     let mut group_count: HashMap<image::Rgb<u8>, usize> = HashMap::new();
     'outer: for &(color, cnt) in count.iter() {
         for (group, val) in group_count.iter_mut() {
-            if dist(color, group) < 32 {
+            if dist(color, group) < distance {
                 *val += cnt;
                 continue 'outer;
             }
@@ -81,10 +95,10 @@ fn main() {
     grouped.sort_by_key(|&(_, v)| v);
     println!("Grouped most common colors");
     for (rank, color) in grouped.iter().rev().enumerate() {
-        if rank >= 8 {
+        if rank >= results {
             break;
         }
-        println!("{:>2}: #{:02x}{:02x}{:02x} {}",
-                 rank + 1, color.0[0], color.0[1], color.0[2], color.1);
+        println!("{:>2}: #{:02x}{:02x}{:02x}",
+                 rank + 1, color.0[0], color.0[1], color.0[2]);
     }
 }
